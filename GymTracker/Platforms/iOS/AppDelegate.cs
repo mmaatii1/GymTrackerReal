e0650175d3973c6e6c63db7d3368ad6dce96dcf2
Mateusz Szreder <mateusz.szreder00@gmail.com>
2023-02-24 19:00:15 +0100

    rest of maiui app

---
+++ GymTracker/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GymTracker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
