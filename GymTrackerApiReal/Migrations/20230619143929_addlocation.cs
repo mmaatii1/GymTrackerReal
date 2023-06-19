073a557ad41c1e09bcf7b5e1ba6c7523fcb79678
Mateusz Szreder <mateusz.szreder00@gmail.com>
2023-06-19 16:39:45 +0200

    add migration

---
+++ GymTrackerApiReal/Migrations/20230619143929_addlocation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymTrackerApiReal.Migrations
+{
+    /// <inheritdoc />
+    public partial class addlocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Location",
+                table: "CustomWorkouts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location",
+                table: "CustomWorkouts");
+        }
+    }
+}
